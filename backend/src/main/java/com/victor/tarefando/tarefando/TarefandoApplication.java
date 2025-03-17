f15c8533db1ae992c69985b05ebc58b018e698b5
Victor Saraiva <victor_alex2005@hotmail.com>
2025-03-17 15:29:25 -0300

    build: inicialização do Spring Boot project com Maven wrapper e dependências básicas

---
+++ backend/src/main/java/com/victor/tarefando/tarefando/TarefandoApplication.java
@@ -0,0 +1,13 @@
+package com.victor.tarefando.tarefando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarefandoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/victor/tarefando/tarefando/TarefandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.tarefando.tarefando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
