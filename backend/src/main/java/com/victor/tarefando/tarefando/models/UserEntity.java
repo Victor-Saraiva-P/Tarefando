b10f14afab65737ed52249ed557df504a2614fbb
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-09 20:38:58 -0300

    feat: refatora UserEntity para incluir enum Role e altera campos de identificação

--- backend/src/main/java/com/victor/tarefando/tarefando/models/UserEntity.java
+++ backend/src/main/java/com/victor/tarefando/tarefando/models/UserEntity.java
@@ -1,82 +1,33 @@
 package com.victor.tarefando.tarefando.models;
 
+import com.victor.tarefando.tarefando.models.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.List;
 
 @Entity
 @Table(name = "users", uniqueConstraints = {
-        @UniqueConstraint(columnNames = "username"),
         @UniqueConstraint(columnNames = "email")
 })
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class UserEntity implements UserDetails {
+public class UserEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String uuid;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
-    @Column(nullable = false, unique = true)
-    private String username;
+    @Column(nullable = false)
+    private String nomeUsuario;
 
     @Column(nullable = false, unique = true)
     private String email;
 
     @ToString.Exclude
     @Column(nullable = false)
-    private String password;
+    private String senha;
 
-    @Builder.Default
-    private boolean enabled = true;
-
-    @Builder.Default
-    private boolean accountNonExpired = true;
-
-    @Builder.Default
-    private boolean accountNonLocked = true;
-
-    @Builder.Default
-    private boolean credentialsNonExpired = true;
-
-    @Column(updatable = false)
-    @CreationTimestamp
-    private LocalDateTime createdAt;
-
-    @UpdateTimestamp
-    private LocalDateTime updatedAt;
-
-    // ----------------- Overrides  -----------------
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return accountNonExpired;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return accountNonLocked;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return credentialsNonExpired;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return enabled;
-    }
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role roles = Role.USER; // valor padrão
 }
