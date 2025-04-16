dc2db029af297998263fc93ebde320bd84203be2
Victor Saraiva <victor_alex2005@hotmail.com>
2025-04-16 12:37:04 -0300

    feat: implementa o mapper para os dtos de user e configura o mapper

--- backend/src/main/java/com/victor/tarefando/tarefando/mappers/impl/user/UserMapperImpl.java
+++ backend/src/main/java/com/victor/tarefando/tarefando/mappers/impl/user/UserMapperImpl.java
@@ -1,11 +1,14 @@
-package com.victor.tarefando.tarefando.mappers.impl;
+package com.victor.tarefando.tarefando.mappers.impl.user;
 
-import com.victor.tarefando.tarefando.dto.UserDTO;
+
+import com.victor.tarefando.tarefando.dtos.user.UserDTO;
 import com.victor.tarefando.tarefando.mappers.Mapper;
 import com.victor.tarefando.tarefando.models.UserEntity;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
 
+@Component
 @AllArgsConstructor
 public class UserMapperImpl implements Mapper<UserEntity, UserDTO> {
     private final ModelMapper modelMapper;
